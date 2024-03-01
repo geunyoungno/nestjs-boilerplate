d48e6abfad78ee59cc53728f05eae35ea61d1246
geunyoungno <geunyoungno@gmail.com>
2024-03-01 14:07:44 +0900

    * change platform to fastify (platform을 fastify로 변경)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+/**
+ * 포트 번호를 가져오는 함수
+ * @returns {number} 포트 번호
+ */
+function getPort() {
+  const envPort = process.env.PORT ?? '';
+  const parsedPort = parseInt(envPort, 10);
+
+  if (Number.isNaN(parsedPort) === false) {
+    return parsedPort;
+  }
+
+  return 3000;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {});
+
+  await app.listen(
+    getPort(),
+    // 외부에서 접속할 수 있도록 하기 위해서 설정
+    '0.0.0.0',
+  );
 }
 bootstrap();
